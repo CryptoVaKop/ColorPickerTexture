b62507bf0c7768f9c3d84f23693f05d5622dcf75
CryptoVaKop <vadikop2@gmail.com>
2019-06-23 01:35:17 +0300

    Add sources

---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+
+/// <summary>
+/// Класс с расширениями.
+/// </summary>
+
+
+public static class Extensions
+{
+    /// <summary>
+    /// Заполнить текстуру заданным цветом.
+    /// </summary>
+    /// <param name="texture"> Ссылка на текстуру. </param>
+    /// <param name="color"> Цвет заполнениия. </param>
+    public static void Fill(this Texture2D texture, Color color)
+    {
+        Color[] colors = new Color[texture.width * texture.height];
+        for (int n = 0; n < colors.Length; n++)
+        {
+            colors[n] = color;
+        }
+
+        texture.SetPixels(colors);
+    }
+
+
+    /// <summary>
+    /// Описание делегата для расчета цвета пикселя, в зависимости от координат центра пикселя.
+    /// </summary>
+    /// <param name="pixelPos"> Координаты центра пикселя относительно центра закрашиваемого круга. </param>
+    /// <returns></returns>
+    public delegate Color CalcColorDelegate(Vector2 pixelPos);
+
+    /// <summary>
+    /// Нарисовать круг на текстуре.
+    /// </summary>
+    /// <param name="texture"> Ссылка на текстуру. </param>
+    /// <param name="centerX"> Координата X центра круга. </param>
+    /// <param name="centerY"> Координата Y центра круга. </param>
+    /// <param name="radius"> Радиус круга. </param>
+    /// <param name="calcColor"> Функция для расчета цвета для каждого пикселя. </param>
+    public static void Circle(this Texture2D texture, int centerX, int centerY, int radius, CalcColorDelegate calcColor)
+    {
+        Color[] colors = texture.GetPixels(centerX - radius, centerY - radius, 2 * radius, 2 * radius);
+
+        for (int y = 0; y < radius; y++)
+        {
+            float pixelPosY = y + 0.5f;
+
+            int py = centerY + y;
+            int ny = centerY - y - 1;
+
+            for (int x = 0; x < radius; x++)
+            {
+                float pixelPosX = x + 0.5f;
+
+                Vector2 pixelPos = new Vector2(pixelPosX, pixelPosY);
+                if (pixelPos.sqrMagnitude < radius * radius)
+                {
+                    int px = centerX + x;
+                    int nx = centerX - x - 1;
+
+                    colors[2 * radius * py + px] = calcColor(pixelPos);
+                    colors[2 * radius * py + nx] = calcColor(new Vector2(-pixelPosX, pixelPosY));
+                    colors[2 * radius * ny + px] = calcColor(new Vector2(pixelPosX, -pixelPos.y));
+                    colors[2 * radius * ny + nx] = calcColor(new Vector2(-pixelPos.x, -pixelPos.y));
+                }
+            }
+        }
+        texture.SetPixels(centerX - radius, centerY - radius, 2 * radius, 2 * radius, colors);
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,130 @@
+﻿using System.IO;
+using UnityEngine;
+
+
+/// <summary>
+/// Программа для создания и сохранения в файл текстуры для ColorPicker-а в формате PNG.
+/// Текстура представляет из себя круг закрашенный смесью цветов RGB
+/// </summary>
+public class Main : MonoBehaviour
+{
+    /// <summary>
+    /// Радиус круга в пикселях.
+    /// </summary>
+    [Tooltip ("Радиус круга в пикселях.")]
+    public int  Radius = 256;
+
+    /// <summary>
+    /// Имя файла в который будет сохранена текстура.
+    /// </summary>
+    [Tooltip ("Имя файла в который будет сохранена текстура.")]
+    public string FileName = "ColorPicker.png";
+
+   /// <summary>
+    /// Массив с координатами точек R, G и B.
+    /// </summary>
+    private Vector2[] RGBpoints = new Vector2[3];
+
+    /// <summary>
+    /// Ссылка на массив под угловые расстояния заданной точки до точек R, G и B соответственно.
+    /// </summary>
+    private float[] Angles;
+
+
+    /// <summary>
+    /// Рассчет цвета пикселя в зависимости от координат его центра. 
+    /// </summary>
+    /// <param name="pixelPos"> Координаты центр пикселя относительно центра закрашиваемого круга. </param>
+    /// <returns> Рассчитанный цвет пикселя. </returns>
+    private Color CalcColor(Vector2 pixelPos)
+    {
+        Color color = Color.black;
+
+        // Рассчитать угловые расстояния от центра пикселя до точек R, G и B соответственно
+        for (int n = 0; n < Angles.Length; n++)
+        {
+            Angles[n] = Vector2.Angle(pixelPos, RGBpoints[n]);
+        }
+
+        // Найти минимальное, максмальное и среднее угловые расстояния
+        float angleMin = Mathf.Min(Angles);
+        float angleMax = Mathf.Max(Angles);
+        float angleMed = 0;
+        for (int n = 0; n < Angles.Length; n++)
+        {
+            if ((Angles[n] != angleMin) && (Angles[n] != angleMax))
+            {
+                angleMed = Angles[n];
+            }
+        }
+
+        // В зависимости от углвых расстояни рассчитать цвет пикселя
+        for (int n = 0; n < Angles.Length; n++)
+        {
+            if (Angles[n] == angleMin)
+            {
+                color[n] = 1.0f;
+            }
+            else if (Angles[n] == angleMax)
+            {
+                color[n] = 0.0f;
+            }
+            else
+            {
+                color[n] = 2 * angleMin / (angleMin + angleMed);
+            }
+        }
+
+        color = Color.Lerp(color, Color.white, (Radius - pixelPos.magnitude) / Radius);
+
+        return color;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Кватернион поворота на 120 градусов по часовой стрелке вокруг оси Z
+        Quaternion rotateQuaternion = Quaternion.Euler(0, 0, -120);
+
+        // Инициализировать массив с координатами точек R, G и B 
+        RGBpoints[0] = new Vector2(0, Radius);
+        for (int p = 1; p < RGBpoints.Length; p++)
+        {
+            RGBpoints[p] = rotateQuaternion * RGBpoints[p - 1];
+        }
+
+        // Создать массив массив под угловые расстояния заданной точки до точек R, G и B
+        Angles = new float[RGBpoints.Length];
+
+        // Создать текстуру
+        Texture2D texture = new Texture2D(2 * Radius, 2 * Radius);
+
+        // Заполнить текстуру прозрачным цветом
+        Color transparentColor = new Color(0, 0, 0, 0);
+        texture.Fill(transparentColor);
+
+        // Нарисовать цветной круг
+        texture.Circle(Radius, Radius, Radius, CalcColor);
+
+        // Применить отрисванные пиксели к текстуре
+        texture.Apply();
+
+        // Преобразовать текстуру в фал PNG и уничтожить текстуру, она нам больше не понадобится
+        byte[] bytes = texture.EncodeToPNG();
+        Destroy(texture);
+
+        // Сохранить ткстуру в файл в папку с текщим проектом
+        File.WriteAllBytes(Application.dataPath + "/../" + FileName, bytes);
+
+        // Вывести в отладку сообщение о том что текстура успешно создана и сохранена в файл 
+        Debug.Log("Текстура успешно создана и сохранена в файл!");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
